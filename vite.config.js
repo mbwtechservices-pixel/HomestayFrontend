4586ee8b7fc867424fe0edfc36a899092094471c
David <mdsmarch14@gmail.com>
2026-02-06 08:16:09 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:9866',
+        // target: 'http://localhost:9866',
+        target: 'https://homestaybackend.onrender.com',
         changeOrigin: true,
       },
     },
